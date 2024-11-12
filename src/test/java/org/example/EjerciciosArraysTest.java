9aaa155659747c40defadfea360cdd4108fc8e7e
jcalata <jcalataruiz@hotmail.com>
2024-11-12 22:44:48 +0100

    Ejercicio buscar Strings comunes acabado 1.0

--- src/test/java/org/example/EjerciciosArraysTest.java
+++ src/test/java/org/example/EjerciciosArraysTest.java
@@ -1,10 +1,8 @@
 package org.example;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class EjerciciosArraysTest {
     EjerciciosArrays cut = new EjerciciosArrays();

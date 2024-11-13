ed2104bee38fa3c7c75dabfc749ecc83f30bd672
curso05 <notiene@notiene.es>
2024-11-13 10:09:53 +0100

    modificado para que retorne los comunes en un array

--- src/main/java/org/example/EjerciciosArrays.java
+++ src/main/java/org/example/EjerciciosArrays.java
@@ -2,22 +2,24 @@ package org.example;
 
 public class EjerciciosArrays {
 
-    public int encontrarComunesEnArray(String [] lista1, String[] lista2) throws Exception{
-        if (lista1.length==0 || lista2.length==0){
+    public String[] encontrarComunesEnArray(String[] lista1, String[] lista2) throws Exception {
+        String [] listaComunes = new String[lista1.length];
+        if (lista1.length == 0 || lista2.length == 0) {
             throw new Exception("Los arrays no puedes estar vacios");
         }
-        
+
         int numeroComunes = 0;
         for (String elemento1 : lista1) {
             for (String elemento2 : lista2) {
                 if (elemento2.equals(elemento1)) {
-                    numeroComunes++;
                     
+                    listaComunes[numeroComunes]=elemento2;
+                    numeroComunes++;
 
                 }
             }
         }
-        return numeroComunes;
+        return listaComunes;
     }
 
 }
--- src/test/java/org/example/EjerciciosArraysTest.java
+++ src/test/java/org/example/EjerciciosArraysTest.java
@@ -1,22 +1,25 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class EjerciciosArraysTest {
     EjerciciosArrays cut = new EjerciciosArrays();
-    
+
     @Test
     public void encontrarComunesEnArrayTest() throws Exception {
 
-        String [] frutas1 = {"patata","manzana","naranja","pera"};
-        String [] frutas2 = {"patata","manzana","naranja","pera", "melon", "sandia", "cereza"};
-        assertEquals(4, cut.encontrarComunesEnArray(frutas1, frutas2));
+        String[] frutas1 = { "patata", "cereza", "naranja", "pera" };
+        String[] frutas2 = { "patata", "manzana", "naranja", "pera", "melon", "sandia", "ciruela" };
+        String[] resultado = { "patata", "naranja", "pera", null };
+        assertArrayEquals(resultado, cut.encontrarComunesEnArray(frutas1, frutas2));
+
     }
+
     @Test
-    public void esTrue(){
+    public void esTrue() {
         assertTrue(true);
     }
 }
